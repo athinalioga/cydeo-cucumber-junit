d10f58b617b1ac49835e93703f9c1cfd417fd6a9
athinal <lioga01@hotmail.com>
2022-06-15 16:31:28 +0100

    Cucumber

---
+++ src/test/java/com/cydeo/pages/WikiSearchPage.java
@@ -0,0 +1,24 @@
+package com.cydeo.pages;
+
+import com.cydeo.utillities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class WikiSearchPage {
+    public WikiSearchPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "searchInput")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement searchButton;
+
+    @FindBy(id = "firstHeading")
+    public WebElement mainHeader;
+
+   @FindBy(xpath = "//div[@class='infobox-caption']")
+    public WebElement imageHeader;
+}
